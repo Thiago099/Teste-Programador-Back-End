82dce9c18adb430f2b0663b2bcd6b116eb725200
Thiago Kaique Ribeiro Fonseca <66787043+Thiago099@users.noreply.github.com>
2020-11-28 15:13:32 -0300

    Update 6 - MySQL.sql

--- 6 - MySQL.sql
+++ 6 - MySQL.sql
@@ -1,3 +1,39 @@
+// Questões de SQL - MySql
+// Considere o seguinte modelo
+CREATE DATABASE tsa_teste_backend_bd;
+USE tsa_teste_backend_bd;
+
+CREATE TABLE organizacao(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(200) NOT NULL,
+    data_fundacao DATE NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO organizacao(nome, data_fundacao) VALUES ('empresa_1', '2020-07-01');
+INSERT INTO organizacao(nome, data_fundacao) VALUES ('empresa_2', '1980-05-12');
+
+CREATE TABLE colaborador(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    organizacao_id INT UNSIGNED NOT NULL,
+    data_nascimento DATE NOT NULL,
+    salario DOUBLE(9,2),
+    PRIMARY KEY(id),
+    FOREIGN KEY (organizacao_id) REFERENCES organizacao (id)
+);
+
+INSERT INTO colaborador(nome, data_nascimento, salario, organizacao_id) 
+VALUES('Alessandra', '1998-02-06', 5000, 1);
+
+INSERT INTO colaborador(nome, data_nascimento, salario, organizacao_id) 
+VALUES('Leandro', '1990-04-09', 1900, 2);
+
+INSERT INTO colaborador(nome, data_nascimento, salario, organizacao_id) 
+VALUES('Bruno', '1987-12-08', 1800, 1);
+
+INSERT INTO colaborador(nome, data_nascimento, salario, organizacao_id) 
+VALUES('Gustavo', '1995-10-17', 2000, 2);
 --a) O nome da organização que foi fundada por ultimo
 SELECT nome AS Organização_fundada_por_ultimo
 FROM organizacao
